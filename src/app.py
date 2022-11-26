5dd4a66bae8d30eeaac5ae3e1ec368bd97ed0ad5
poncianogr <poncianogr@gmail.com>
2022-11-26 14:37:11 +0000

    family api

--- src/app.py
+++ src/app.py
@@ -6,21 +6,37 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+member1 = { "id" : jackson_family._generateId(),
+            "first_name" :"John", 
+            "last_name": jackson_family.last_name,
+            "age" : 33, 
+            "lucky_numbers": [7, 13, 22]}
+member2 = { "id" : jackson_family._generateId(),
+            "first_name" :"Jane", 
+            "last_name": jackson_family.last_name,
+            "age" : 35, 
+            "lucky_numbers": [10, 14, 3]}
+member3 = { "id" : jackson_family._generateId(),
+            "first_name" :"Jimmy", 
+            "last_name": jackson_family.last_name,
+            "age" : 5, 
+            "lucky_numbers": [1]}
+jackson_family.add_member(member1)
+jackson_family.add_member(member2)
+jackson_family.add_member(member3)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
@@ -28,17 +44,31 @@ def sitemap():
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
-    # this is how you can use the Family datastructure by calling its methods
+    
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if "msg" in member : return jsonify(member), 400
+    return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.get_json()
+    if member["age"] <= 0: return jsonify({"msg":"Age must be over 0"}), 400
+    jackson_family.add_member(member)
+    return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if "msg" in member : return jsonify(member), 400
+    member["done"] = True
+    return jsonify(member), 200
 
 
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,40 @@
 
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        # example list of members
+        
         self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+    
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        
+        member["last_name"] = self.last_name
+        if "id" == None in member : member["id"] = self._generateId()
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+       
+        for i in range(len(self._members)):
+            if self._members[i]['id'] == id:
+                aux = self._members[i]
+                del self._members[i]
+                return aux
+        return {"msg" : "Id not found"}
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        
+        for i in range(len(self._members)):
+            if self._members[i]['id'] == id:
+                return self._members[i]
+        return {"msg" : "Id not found"}
 
-    # this method is done, it returns a list with all the family members
+    
     def get_all_members(self):
         return self._members
